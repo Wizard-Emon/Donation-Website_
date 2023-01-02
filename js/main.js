f0705b2e21f2522a6c47d786e2e2ff967832a761
Emam Ul Haque Emon <emam5316@gmail.com>
2023-01-02 23:09:32 +0600

    last

--- js/main.js
+++ js/main.js
@@ -25,3 +25,16 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+$(document).ready(function() {
+    $(".read").click(function() {
+        $(this).prev().toggle();
+        if ($(this).text() == 'read less') {
+            $(this).text('read more')
+        } else {
+            $(this).text('read less')
+
+        }
+    })
+});
